45a00e63c9dbcb80a3ae7dcb6de241e7306dffba
Ruslan <shamil.gatiatullin@yandex.ru>
2025-04-16 14:28:31 +0800

    Added docker

--- sender/sender.py
+++ sender/sender.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import requests
 from time import sleep
-
 # Python script that reads .csv file and send the packages to a Flask server 
+
 df = pd.read_csv('ip_addresses.csv')
 df.sort_values(by=['Timestamp'], inplace=True)
 df.rename({'ip address': 'ip'}, axis=1, inplace=True)
@@ -13,5 +13,5 @@ for i in range(df.size // df.columns.size):
     if prev != curr_time:
         sleep((curr_time - prev) / 1000)
         prev = curr_time
-    response = requests.post(url='http://127.0.0.1:8080/new_connection', json=df.loc[i].to_json())
+    response = requests.post(url='http://server:8080/new_connection', json=df.loc[i].to_json())
 print('done') 
